baa585c240cb1aad845b592dbc7559a0aae5788f
delyan-kirov <delyan.k.kirov@gmail.com>
2023-09-17 22:20:47 +0300

    Addings new scripts.

---
+++ experiments/scripts/make.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/bash
+
+conjure solve -ac --number-of-solutions=all --solver=nbc_minisat_all ../models/mixed/avoidance/mixedVincular.essence ../params/mixed/avoidance/Vincular/132_1.param 
+
