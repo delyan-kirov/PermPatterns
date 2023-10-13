5664c6365641ed2a6f638aa80e341a8961e56324
delyan-kirov <delyan.k.kirov@gmail.com>
2023-10-13 16:49:41 +0300

    Adding new experiment

--- experiment/meshMixed.sh
+++ experiment/meshMixed.sh
@@ -1,26 +1,31 @@
 #!/usr/bin/bash
 
-mixedBivincModel=../models/mixed/avoidance/bivincular.essence
-mixedVincModel=../models/mixed/avoidance/vincular.essence
-mixedConsecModel=../models/mixed/avoidance/consecutive.essence
-mixedBoxModel=../models/mixed/avoidance/boxed.essence
+mixedBivincAvoidModel=../models/mixed/avoidance/bivincular.essence
+mixedVincAvoidModel=../models/mixed/avoidance/vincular.essence
+mixedConsecAvoidModel=../models/mixed/avoidance/consecutive.essence
+mixedBoxAvoidModel=../models/mixed/avoidance/boxed.essence
+
+mixedAvoidModelDir=../models/mixed/avoidance/
 
 mixedVincAvoidParams=../params/mixed/avoidance/vincular/
 mixedBivincAvoidParams=../params/mixed/avoidance/bivincular/
 mixedConsecAvoidParams=../params/mixed/avoidance/consecutive/
 mixedBoxAvoidParams=../params/mixed/avoidance/boxed/
 
+results=../results
 
-cat $mixedBoxModel
-echo 
-cat $mixedVincModel
-echo 
-cat $mixedConsecModel
-echo 
-cat $mixedBivincModel
-echo 
+count=1
 
-ls $mixedVincAvoidParams 
-ls $mixedBivincAvoidParams
-ls $mixedConsecAvoidParams
-ls $mixedBoxAvoidParams
+for file in "$mixedBivincAvoidParams"*; do
+  #solve patterns
+  conjure solve -ac --number-of-solutions=all --solver=nbc_minisat_all $mixedBivincAvoidModel $file
+  find $mixedAvoidModelDir -type f -name "*solution" -delete
+  #store results from the solving process
+  mv -T conjure-output "conjure-output-$count"
+  mv "conjure-output-$count" $results
+  count=$((count+1)) 
+done
+
+count=1
+
+
